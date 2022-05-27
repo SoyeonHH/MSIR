0fa5af78a2903fba3d7d1530158f2659ce1662d0
SoyeonHH <sodus1102@gmail.com>
2022-05-27 03:33:29 +0000

    Sentiment Intensity BarGraph Observation

--- MMIM/test_instance.py
+++ MMIM/test_instance.py
@@ -49,7 +49,7 @@ class TestMOSEI(object):
         self.model = model
 
     def start(self):
-        sys.stdout = open('/mnt/soyeon/workspace/multimodal/MISA/MISA_mosei.txt', 'w')
+        # sys.stdout = open('/mnt/soyeon/workspace/multimodal/MISA/MISA_mosei.txt', 'w')
         self.model.eval()
         segment_list = []
         words_list = []
@@ -172,7 +172,7 @@ class TestMOSEI(object):
             print(preds_2[i])
             print(preds_7[i])
 
-        sys.stdout.close()
+        # sys.stdout.close()
 
 
 class InputFeatures(object):
@@ -288,7 +288,7 @@ class TestMOSI(object):
         self.model = model
 
     def start(self):
-        sys.stdout = open('/mnt/soyeon/workspace/multimodal/MMIM/MMIM_mosi.txt', 'w')
+        # sys.stdout = open('/mnt/soyeon/workspace/multimodal/MMIM/MMIM_mosi.txt', 'w')
         self.model.eval()
         segment_list = []
         preds = []
@@ -354,7 +354,7 @@ class TestMOSI(object):
             print(preds_2[i])
             print(preds_7[i])
 
-        sys.stdout.close()
+        # sys.stdout.close()
 
         return segment_list, preds, preds_2, preds_7
 
