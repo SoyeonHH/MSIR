fef8604a517c618d629032f0b54a414e1b193ded
wooyeon <qt5828@naver.com>
2022-09-26 21:45:42 +0900

    Add train/model/main for Unimodal & Modify run.sh for Unimodal

--- MulT/run.sh
+++ MulT/run.sh
@@ -1,5 +1,6 @@
 ## MOSI unaligned
-python main.py --model MULT --dataset mosi --data_path /data1/multimodal/MulT/data --batch_size 128 --num_heads 10 --embed_dropout 0.2 --attn_dropout 0.2 --out_dropout 0.1 --num_epochs 100
+## modality = 'text' or 'audio' or 'visual'
+python main_unimodal.py --dataset mosi --data_path /data1/multimodal/MulT/data --vonly --aonly --lonly --batch_size 32 --num_heads 10 --embed_dropout 0.2 --attn_dropout 0.2 --out_dropout 0.1 --num_epochs 100 --modality 'visual' 
 
 ## MOSI aligned
 # python main.py --dataset mosi --data_path /data1/multimodal/MulT/data --vonly --aonly --lonly --batch_size 32 --num_heads 10 --embed_dropout 0.2 --attn_dropout 0.2 --out_dropout 0.1 --num_epochs 100 --aligned
