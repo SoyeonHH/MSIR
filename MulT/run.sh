e5ac090cebc31ca6d6d77c53f59aba93bf9ff6ae
SoyeonHH <sodus1102@gmail.com>
2022-10-06 15:08:34 +0900

    Update MulT experiments

--- MulT/run.sh
+++ MulT/run.sh
@@ -4,7 +4,7 @@
 # python main.py --dataset mosi --data_path /data1/multimodal/processed_data --batch_size 32 --num_heads 10 --embed_dropout 0.2 --attn_dropout 0.2 --out_dropout 0.1 --num_epochs 100
 
 ## MOSEI unaligned
-python main.py --dataset mosei_senti --data_path /data1/multimodal/processed_data --batch_size 16 --num_heads 10 --embed_dropout 0.3 --attn_dropout 0.1 --out_dropout 0.1 --num_epochs 20
+# python main.py --dataset mosei_senti --data_path /data1/multimodal/processed_data --batch_size 16 --num_heads 10 --embed_dropout 0.3 --attn_dropout 0.1 --out_dropout 0.1 --num_epochs 20
 
 ## MOSI aligned
 # python main.py --dataset mosi --data_path /data1/multimodal/processed_data --batch_size 32 --num_heads 10 --embed_dropout 0.2 --attn_dropout 0.2 --out_dropout 0.1 --num_epochs 100 --aligned
@@ -24,3 +24,12 @@ python main.py --dataset mosei_senti --data_path /data1/multimodal/processed_dat
 
 ## MOSI - audio
 # python unimodal/main.py --dataset mosi --data_path /data1/multimodal/processed_data --batch_size 32 --num_heads 10 --embed_dropout 0.2 --attn_dropout 0.2 --out_dropout 0.1 --num_epochs 100 --modality 'audio'
+
+## MOSEI - text
+python unimodal/main.py --dataset mosei_senti --data_path /data1/multimodal/processed_data --batch_size 16 --num_heads 10 --embed_dropout 0.3 --attn_dropout 0.1 --out_dropout 0.1 --num_epochs 20 --modality 'text'
+
+## MOSEI - visual
+python unimodal/main.py --dataset mosei_senti --data_path /data1/multimodal/processed_data --batch_size 16 --num_heads 10 --embed_dropout 0.3 --attn_dropout 0.1 --out_dropout 0.1 --num_epochs 20 --modality 'visual'
+
+## MOSEI - audio
+python unimodal/main.py --dataset mosei_senti --data_path /data1/multimodal/processed_data --batch_size 16 --num_heads 10 --embed_dropout 0.3 --attn_dropout 0.1 --out_dropout 0.1 --num_epochs 20 --modality 'audio'
